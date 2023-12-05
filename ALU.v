eb35dd90d2cf43186e96850e58eeb529c181484a
MeoLamBao <147321028+MeoLamBao@users.noreply.github.com>
2023-12-05 18:28:27 +0700

    ALU.v

--- ALU.v
+++ ALU.v
@@ -1,9 +1,9 @@
+`timescale 1ns/1ps
 module ALU(
 
 input [31:0] inA, inB,
 input [3:0] ALU_control,
-output reg [31:0] Result,
-output reg Zero_F, Sign_F
+output reg [31:0] Result
 
 );
 
@@ -15,25 +15,16 @@ always @(*)
 			4'b0000: Result = inA + inB;	//A+B
 			4'b0001: Result = inA - inB;	//A-B
 			4'b0010: Result = inA & inB;	//A and B
-			4'b0100: Result = inA | inB;	//A or b
-			4'b1000: begin	//set on less than
-				if (inA < inB) 
-					Result = 1;
-				else
-					Result = 0;
-			end
-			4'b0011: Result = inA << inB;	//shift left
+			4'b0011: Result = inA | inB;	//A or B
+			4'b0100: Result = inA << inB;	//shift left
 			4'b0101: Result = inA >> inB;	//shift right
 			4'b0110: Result = inA >>> inB; 	//A>>>B
 			4'b0111: Result = inA ^ inB;	//A xor B
-			
+			4'b1000:	Result = ($signed(inA) < $signed(inB)) ? 1 : 0;	//SLT
+			4'b1001: Result = (inA < inB) ? 1 : 0; // SLTU
+			4'b1010: Result = inB; // lui
+			default : Result = 31'd0;
 		endcase
-		
-		if (Result == 0) 
-			ZF = 1'b1;
-		else
-			ZF = 1'b0;
-		
-		SF = Result[31];
 	end
+		
 endmodule
