20e91b0812993313aab9c485998469f08ed19e86
MeoLamBao <147321028+MeoLamBao@users.noreply.github.com>
2023-12-05 18:29:38 +0700

    Data_memory.v

--- Data_memory.v
+++ Data_memory.v
@@ -1,20 +1,26 @@
-module Data_memory(
-input clk, En_RW,
-input [31:0] Address,
-input [31:0] Write_Data,
-output reg [31:0] Read_Data
+`timescale 1ns/1ps
+module Data_memory (
+    input CLK, RST,  
+    input [31:0] A, // ALU_OUT = memory address
+    input [31:0] WD,
+    input [1:0] RWE,
+    output [31:0] RD
 );
 
-reg [31:0] mem [31:0]
+    reg [31:0] MEMORY [63:0];
+    integer i;
 
-always @(posedge clk)
-
-begin
-	if (En_RW) begin
-		mem[Address] = Write_Data;
-	end
-	else begin
-		Read_Data = mem[Address];
-	end
-end
-endmodule
+    always @(posedge CLK or posedge RST) begin
+        if (RST) begin
+           for (i = 0; i < 64 ; i = i + 1) begin
+                MEMORY[i] = 32'b0;
+           end
+        end
+        else begin
+            if (RWE == 1)  MEMORY[A] <= {{24{WD[7]}}, WD[7:0]}; // SB instruction
+            else if (RWE == 2)  MEMORY[A] <= {{16{WD[7]}}, WD[15:0]}; // SH instruction
+            else if (RWE == 3)  MEMORY[A] <= WD; // SW instruction
+        end
+    end
+    assign  RD = MEMORY[A];
+endmodule 
